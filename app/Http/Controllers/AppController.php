52165be774731920a50a4cdeea639e0ddc9da60c
moukim hfaidh <moukimhfaid@gmail.com>
2021-03-24 02:49:43 +0100

    finished

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Course;
 use Illuminate\Http\Request;
 
 class AppController extends Controller
 {
     public function index (){
-        return view('library');
+        return view('library',['courses' => Course::all()]);
     }
 }
